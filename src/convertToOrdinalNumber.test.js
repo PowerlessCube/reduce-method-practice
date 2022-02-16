a71fbfd676d157ca143250c8c4be4b1393e26d79
Alistair Mackay <alistair.mackay.work@gmail.com>
2022-02-16 02:23:00 +0000

    changed tests to use test.each(cases)

--- src/convertToOrdinalNumber.test.js
+++ src/convertToOrdinalNumber.test.js
@@ -1,39 +1,18 @@
 import { convertToOrdinalNumber } from './convertToOrdinalNumber';
-
+const cases = [
+  { number: 1, expected: '1st' },
+  { number: 2, expected: '2nd' },
+  { number: 3, expected: '3rd' },
+  { number: 4, expected: '4th' },
+  { number: 101, expected: '101st' },
+  { number: 999999, expected: '999999th' },
+  { number: undefined, expected: undefined },
+  { number: null, expected: null },
+  { number: 0, expected: 0 },
+  { number: -1, expected: -1 },
+];
 describe('ordinal Number', () => {
-  test('1 should be 1st', () => {
-    expect(convertToOrdinalNumber(1)).toBe('1st');
-  });
-
-  test('2 should be 2nd', () => {
-    expect(convertToOrdinalNumber(2)).toBe('2nd');
-  });
-
-  test('3 should be 3rd', () => {
-    expect(convertToOrdinalNumber(3)).toBe('3rd');
-  });
-
-  test('4 should be 4th', () => {
-    expect(convertToOrdinalNumber(4)).toBe('4th');
-  });
-
-  test('101 should be 101st', () => {
-    expect(convertToOrdinalNumber(101)).toBe('101st');
-  });
-
-  test('undefined should return undefined', () => {
-    expect(convertToOrdinalNumber(undefined)).toBe(undefined);
-  });
-
-  test('null should return null', () => {
-    expect(convertToOrdinalNumber(null)).toBe(null);
-  });
-
-  test('0 should return 0', () => {
-    expect(convertToOrdinalNumber(0)).toBe(0);
-  });
-
-  test('-1 should return -1', () => {
-    expect(convertToOrdinalNumber(-1)).toBe(-1);
+  test.each(cases)('$number should be $expected', ({ number, expected }) => {
+    expect(convertToOrdinalNumber(number)).toBe(expected);
   });
 });
